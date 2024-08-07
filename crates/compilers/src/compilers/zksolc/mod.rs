227975210a574af4ef34e1ad8efb724d8b1b0375
elfedy <federico@moonsonglabs.com>
2024-08-07 18:20:23 -0300

    Bring back logs where they should

--- crates/compilers/src/compilers/zksolc/mod.rs
+++ crates/compilers/src/compilers/zksolc/mod.rs
@@ -400,7 +400,7 @@ fn compiler_blocking_install(
     label: &str,
 ) -> Result<PathBuf> {
     use foundry_compilers_core::utils::RuntimeOrHandle;
-    warn!("blocking installing {label}");
+    trace!("blocking installing {label}");
     //trace!("blocking installing {label}");
     // An async block is used because the underlying `reqwest::blocking::Client` does not behave
     // well inside of a Tokio runtime. See: https://github.com/seanmonstar/reqwest/issues/1017
@@ -417,21 +417,20 @@ fn compiler_blocking_install(
                 .bytes()
                 .await
                 .map_err(|e| SolcError::msg(format!("failed to download {label} file: {e}")))?;
-            warn!("downloaded {label}");
-            //trace!("downloaded {label}");
+            trace!("downloaded {label}");
 
             // lock file to indicate that installation of this compiler version will be in progress.
             // wait until lock file is released, possibly by another parallel thread trying to install the
             // same compiler version.
-            warn!("try to get lock for {label}");
+            trace!("try to get lock for {label}");
             let _lock = try_lock_file(lock_path)?;
-            warn!("got lock for {label}");
+            trace!("got lock for {label}");
 
             // Only write to file if it is not there. The check is doneafter adquiring the lock
             // to ensure the thread remains blocked until the required compiler is
             // fully installed
             if !compiler_path.exists() {
-                warn!("creating binary for {label}");
+                trace!("creating binary for {label}");
                 //trace!("creating binary for {label}");
                 let mut output_file = File::create(&compiler_path).map_err(|e| {
                     SolcError::msg(format!("Failed to create output {label} file: {e}"))
@@ -445,7 +444,7 @@ fn compiler_blocking_install(
                     SolcError::msg(format!("Failed to set {label} permissions: {e}"))
                 })?;
             } else {
-                warn!("found binary for {label}");
+                trace!("found binary for {label}");
             }
         } else {
             return Err(SolcError::msg(format!(
@@ -453,7 +452,7 @@ fn compiler_blocking_install(
                 response.status()
             )));
         }
-        warn!("{label} instalation completed");
+        trace!("{label} instalation completed");
         Ok(compiler_path)
     })
 }
